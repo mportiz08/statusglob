2451f6b2dfa6ab424a52e7c36b579f5da13d45a1
Marcus Ortiz <mportiz08@gmail.com>
2010-07-27 17:35:54 -0700

    added user_session model/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  # TODO: add root route
+  
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  
+  map.resources :user_sessions
   map.resources :users
+  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
