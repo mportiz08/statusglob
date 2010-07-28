338f8ab7c0b7598bce660bc211a2c1f898e3c0b8
Marcus Ortiz <mportiz08@gmail.com>
2010-07-27 17:30:25 -0700

    added users controller/resource

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Registration successful."
+      redirect_to root_url
+    else
+      render :action => "new"
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
