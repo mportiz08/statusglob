0e5c16d2807460dc7285cb874ef6cd7369f185a3
Marcus Ortiz <mportiz08@gmail.com>
2010-08-02 16:10:26 -0700

    succesfully retrieving tweets

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,13 @@
+require 'json'
+require 'oauth'
+
 class MainController < ApplicationController
   def index
+    if current_user
+      request = "/statuses/home_timeline.json"
+      access = current_user.twitter_account
+      access_token = OAuth::AccessToken.new(consumer("twitter"), access.token, access.secret)
+      @response = JSON.parse(access_token.get(request).body)
+    end
   end
-
 end
