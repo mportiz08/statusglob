25419a171ce7d129b906b92190a0905939f073a8
Marcus Ortiz <mportiz08@gmail.com>
2010-08-02 15:33:58 -0700

    very basic oauth implementation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,53 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   helper_method :current_user
   
+  def connect
+    @request_token = consumer(params["account"]).get_request_token
+    session[:request_token] = @request_token.token
+    session[:request_token_secret] = @request_token.secret
+    # send to site to authorize
+    redirect_to @request_token.authorize_url
+    return
+  end
+
+  def twitter_callback
+    consumer = consumer("twitter")
+    @request_token = OAuth::RequestToken.new(consumer, session[:request_token], session[:request_token_secret])
+    # Exchange the request token for an access token.
+    @access_token = @request_token.get_access_token(:oauth_verifier => params[:id])
+    @response = consumer.request(:get, "/account/verify_credentials.json", @access_token, { :scheme => :query_string })
+    case @response
+    when Net::HTTPSuccess
+      user_info = JSON.parse(@response.body)
+      unless user_info['screen_name']
+        flash[:notice] = "Authentication failed"
+        redirect_to :action => :index
+        return
+      end
+      # We have an authorized user, save the information to the database.
+      @account = TwitterAccount.new({ :user_id => current_user.id, :token => @access_token.token, :secret => @access_token.secret })
+      @account.save!
+      # Redirect to the show page
+      redirect_to(current_user)
+    else
+      RAILS_DEFAULT_LOGGER.error "Failed to get user info via OAuth"
+      # The user might have rejected this application. Or there was some other error during the request.
+      flash[:notice] = "Authentication failed"
+      redirect_to :action => :index
+      return
+    end
+  end
+  
   private
+  
+  def settings(account)
+    settings = YAML::load(File.open("#{Rails.root}/config/accounts.yml"))[account]
+  end
+  
+  def consumer(account)
+    settings = settings(account)
+    OAuth::Consumer.new(settings["consumer_key"], settings["consumer_secret"], {:site=>settings["site"]})
+  end
   
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 require 'oauth'
-require 'yml'
+require 'yaml'
 
 class UsersController < ApplicationController
   def new
@@ -32,54 +32,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end
-
-def connect
-  @request_token = consumer(params["account").get_request_token
-  session[:request_token] = @request_token.token
-  session[:request_token_secret] = @request_token.secret
-  # send to site to authorize
-  redirect_to @request_token.authorize_url
-  return
-end
-
-def twitter_callback
-  consumer = consumer("twitter")
-  @request_token = OAuth::RequestToken.new(consumer, session[:request_token], session[:request_token_secret])
-  # Exchange the request token for an access token.
-  @access_token = @request_token.get_access_token
-  @response = consumer.request(:get, "/account/verify_credentials.json", @access_token, { :scheme => :query_string })
-  case @response
-    when Net::HTTPSuccess
-      user_info = JSON.parse(@response.body)
-      unless user_info['screen_name']
-        flash[:notice] = “Authentication failed”
-        redirect_to :action => :index
-        return
-      end
-      # We have an authorized user, save the information to the database.
-      @account = TwitterAccount.new({ :token => @access_token.token, :secret => @access_token.secret })
-      @account.save!
-      # Redirect to the show page
-      redirect_to(@user)
-    else
-      RAILS_DEFAULT_LOGGER.error “Failed to get user info via OAuth”
-      # The user might have rejected this application. Or there was some other error during the request.
-      flash[:notice] = “Authentication failed”
-      redirect_to :action => :index
-      return
-    end
-  end
-end
-  
-  private
-  
-  def settings(account)
-    settings = YAML::load(File.open("#{Rails.root}/config/accounts.yml"))[account]
-  end
-  
-  def consumer(account)
-    settings = settings(account)
-    OAuth::Consumer.new(settings["consumer_key"], settings["consumer_secret"], {:site=>settings["site"]})
   end
 end
