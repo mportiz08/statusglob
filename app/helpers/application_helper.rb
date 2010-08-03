52245d587b9ca275b48f62e4ff46612bd114c673
Marcus Ortiz <mportiz08@gmail.com>
2010-08-03 16:19:21 -0700

    starting work on front-end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def display_tweet(tweet)
+    @html = "<div class=\"author\">#{tweet.username} <span class=\"timestamp\">said #{time_ago_in_words(tweet.date_posted)} ago</span></div>
+             <div class=\"message\">#{tweet.content}</div>"
+    auto_link(@html, :all, :target => '_blank')
+  end
 end
