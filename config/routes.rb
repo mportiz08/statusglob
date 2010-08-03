1c378f58768e73276db79ad813d8ec98b3951fc8
Marcus Ortiz <mportiz08@gmail.com>
2010-08-02 17:23:55 -0700

    singularized user route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ ActionController::Routing::Routes.draw do |map|
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
   
   map.resources :user_sessions
-  map.resources :users
+  map.resources :user
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
