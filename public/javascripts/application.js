d4570c7e6a5de33e21df86a46296e0cab25b939d
Marcus Ortiz <mportiz08@gmail.com>
2010-08-05 20:30:18 -0700

    restructured header sections

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-  setTimeout(fadeNotification, 3000);
-  
-  function fadeNotification() {
-    $('#flash').fadeOut();
-  }
+  init();
 });
+
+function init() {
+  // fade in main content
+  $('#content').hide();
+  $('#content').fadeIn("slow");
+  
+  // fade out flash messages
+  setTimeout(fadeNotification, 3000);
+}
+
+function fadeNotification() {
+  $('#flash').fadeOut();
+}
