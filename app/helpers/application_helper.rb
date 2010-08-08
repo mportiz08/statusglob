91fe08ec33a83f4d1238fd7007e4685454619fef
Marcus Ortiz <mportiz08@gmail.com>
2010-08-08 00:57:48 -0700

    added app name helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include TwitterHelper
+  
+  def app_name
+    "social *"
+  end
 end
