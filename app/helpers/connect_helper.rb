c8361785ace247ae056b04e0a04d94795a290c88
Marcus Ortiz <mportiz08@gmail.com>
2010-08-08 12:12:19 -0700

    added facebook to connections page

--- app/helpers/connect_helper.rb
+++ app/helpers/connect_helper.rb
@@ -1,10 +1,21 @@
 module ConnectHelper
   def connect_twitter(user)
     "<li id=\"connection-twitter\">
-      <div class=\"message-left\">#{icon(user)}</div>
+      <div class=\"message-left\">#{twitter_icon(user)}</div>
       <div class=\"message-right\">
-        <div class=\"author\">twitter <span class=\"timestamp\">#{added_text(user)}</span></div>
-        <div class=\"message\">#{connected_text(user)}</div>
+        <div class=\"author\">twitter <span class=\"timestamp\">#{twitter_added_text(user)}</span></div>
+        <div class=\"message\">#{twitter_connected_text(user)}</div>
+      </div>
+      <div class=\"clear\"></div>
+    </li>"
+  end
+  
+  def connect_facebook(user)
+    "<li id=\"connection-facebook\">
+      <div class=\"message-left\">#{facebook_icon(user)}</div>
+      <div class=\"message-right\">
+        <div class=\"author\">facebook <span class=\"timestamp\">#{facebook_added_text(user)}</span></div>
+        <div class=\"message\">#{facebook_connected_text(user)}</div>
       </div>
       <div class=\"clear\"></div>
     </li>"
@@ -12,15 +23,29 @@ module ConnectHelper
   
   private
   
-  def icon(user)
+  # twitter helpers
+  def twitter_icon(user)
     (user.twitter?) ? image_tag("icon_twitter.png") : image_tag("icon-bw_twitter.png")
   end
   
-  def added_text(user)
+  def twitter_added_text(user)
     (user.twitter?) ? "added #{time_ago_in_words(user.twitter_account.created_at)} ago" : "not added"
   end
   
-  def connected_text(user)
+  def twitter_connected_text(user)
     (user.twitter?) ? "<span class=\"connected\">connected</span>" : "#{link_to("click to connect", :action => "twitter")}"
+  end
+  
+  # facebook helpers
+  def facebook_icon(user)
+    (user.facebook?) ? image_tag("icon_facebook.png") : image_tag("icon-bw_facebook.png")
+  end
+  
+  def facebook_added_text(user)
+    (user.facebook?) ? "added #{time_ago_in_words(user.facebook_account.created_at)} ago" : "not added"
+  end
+  
+  def facebook_connected_text(user)
+    (user.facebook?) ? "<span class=\"connected\">connected</span>" : "#{link_to("click to connect", :action => "facebook")}"
   end
 end
