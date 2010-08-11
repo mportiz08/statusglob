84783fd7f55fe2568f13c52b572606f1b5a028ec
Marcus Ortiz <mportiz08@gmail.com>
2010-08-11 13:23:45 -0700

    added user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_username(params[:username]) || User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ ActionController::Routing::Routes.draw do |map|
   
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.connect ':username', :controller => "users", :action => "show"
 end
