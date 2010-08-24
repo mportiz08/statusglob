157e283624ac7030aae6c2183550a366e28a2ec8
Marcus Ortiz <mportiz08@gmail.com>
2010-08-23 21:46:48 -0700

    rewriting main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,16 +1,30 @@
 class MainController < ApplicationController
   def index
     @messages = []
+    @tweets = []
+    @statuses = []
+    @stories = []
+    @bookmarks = []
     if current_user
-      flash[:error] = "twitter might be down" unless current_user.update_tweets
-      flash[:error] = "facebook might be down" unless current_user.update_statuses
-      flash[:error] = "digg might be down" unless current_user.update_stories
-      flash[:error] = "delicious might be down" unless current_user.update_bookmarks
+      if current_user.twitter?
+        current_user.update_tweets
+        @tweets << current_user.tweets.all(:order => "date_posted DESC", :limit => 10)
+      end
       
-      @tweets = current_user.tweets.all(:order => "date_posted DESC", :limit => 10) || []
-      @statuses = current_user.statuses.all(:order => "date_posted DESC", :limit => 10) || []
-      @stories = current_user.stories.all(:order => "date_posted DESC", :limit => 5) || []
-      @bookmarks = current_user.bookmarks.all(:order => "date_posted DESC", :limit => 5) || []
+      if current_user.facebook?
+        current_user.update_statuses
+        @statuses << current_user.statuses.all(:order => "date_posted DESC", :limit => 10)
+      end
+      
+      if current_user.digg?
+        current_user.update_stories
+        @stories << current_user.stories.all(:order => "date_posted DESC", :limit => 5)
+      end
+      
+      if current_user.delicious?
+        current_user.update_bookmarks
+        @bookmarks << current_user.bookmarks.all(:order => "date_posted DESC", :limit => 5)
+      end
       
       @messages << (@tweets + @statuses + @stories + @bookmarks)
       @messages = @messages.sort_by { |m| m.date_posted }
