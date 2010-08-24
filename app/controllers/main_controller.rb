ec296caae4ef6b502b79b135a46b18c1033259e6
Marcus Ortiz <mportiz08@gmail.com>
2010-08-23 22:03:23 -0700

    trying to fix duplicate message bug

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,25 +8,25 @@ class MainController < ApplicationController
     if current_user
       if current_user.twitter?
         current_user.update_tweets
-        @tweets << current_user.tweets.all(:order => "date_posted DESC", :limit => 10)
+        @tweets = current_user.tweets.all(:order => "date_posted DESC", :limit => 10)
       end
       
       if current_user.facebook?
         current_user.update_statuses
-        @statuses << current_user.statuses.all(:order => "date_posted DESC", :limit => 10)
+        @statuses = current_user.statuses.all(:order => "date_posted DESC", :limit => 10)
       end
       
       if current_user.digg?
         current_user.update_stories
-        @stories << current_user.stories.all(:order => "date_posted DESC", :limit => 5)
+        @stories = current_user.stories.all(:order => "date_posted DESC", :limit => 5)
       end
       
       if current_user.delicious?
         current_user.update_bookmarks
-        @bookmarks << current_user.bookmarks.all(:order => "date_posted DESC", :limit => 5)
+        @bookmarks = current_user.bookmarks.all(:order => "date_posted DESC", :limit => 5)
       end
       
-      @messages << (@tweets + @statuses + @stories + @bookmarks)
+      @messages = (@tweets + @statuses + @stories + @bookmarks)
       @messages = @messages.sort_by { |m| m.date_posted }
       @messages.reverse!
     end
